8ef99b1716382ea65412c17096f8298f4ff190ef
Sean <xsmpx09@hotmail.com>
2018-12-07 23:32:13 -0500

    updated...

---
+++ files02.cpp
@@ -0,0 +1,11 @@
+//
+// Created by xsilentcoderx on 12/7/2018.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hi! It\'s me. \n";
+    cout << "I\'m learning to program! \n";
+    return 0;
+}
